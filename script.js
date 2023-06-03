7fbfa62ecd1e3a9291df06d3ae1f76d57fc6d7c5
Igor Stepanov <stipyk1309@gmail.com>
2023-06-03 15:17:08 +0300

    display movements

--- script.js
+++ script.js
@@ -58,10 +58,18 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach((mov, i) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+	<div class="movements__row">
+		<div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+		<div class="movements__value">${mov} $</div>
+	</div>`;
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
