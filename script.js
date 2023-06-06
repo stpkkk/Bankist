249ece4dbf6c9236c2c095c012bfc9ea6e3143b7
Igor Stepanov <stipyk1309@gmail.com>
2023-06-06 17:20:27 +0300

    calcDisplayBalance, calcDisplaySummary functions

--- script.js
+++ script.js
@@ -74,6 +74,36 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => {
+    return (acc += mov);
+  }, 0);
+  labelBalance.textContent = `${balance} $`;
+};
+
+calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes} $`;
+
+  const outcomes = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(outcomes)} $`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => int >= 1) //calc only if interest is higher than 1
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest} $`;
+};
+
+calcDisplaySummary(account1.movements);
+
 //side effect with creating an username in accounts obj
 const createUsernames = accs =>
   accs.forEach(
@@ -86,5 +116,3 @@ const createUsernames = accs =>
   );
 
 createUsernames(accounts);
-
-console.log(accounts);
