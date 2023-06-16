2170ed704d5aef6ac449f13a32a883ae8e386bf2
Igor Stepanov <stipyk1309@gmail.com>
2023-06-16 20:54:56 +0300

    Current and movements dates add

--- script.js
+++ script.js
@@ -7,6 +7,19 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2021-11-18T21:31:17.178Z',
+    '2021-12-23T07:42:02.383Z',
+    '2022-01-28T09:15:04.904Z',
+    '2022-04-01T10:17:24.185Z',
+    '2022-05-08T14:11:59.604Z',
+    '2022-05-27T17:01:17.194Z',
+    '2022-07-11T23:36:17.929Z',
+    '2022-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT',
 };
 
 const account2 = {
@@ -14,6 +27,19 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2021-11-01T13:15:33.035Z',
+    '2021-11-30T09:48:16.867Z',
+    '2021-12-25T06:04:23.907Z',
+    '2022-01-25T14:18:46.235Z',
+    '2022-02-05T16:33:06.386Z',
+    '2022-04-10T14:43:26.374Z',
+    '2022-06-25T18:49:59.371Z',
+    '2022-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -21,6 +47,19 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+
+  movementsDates: [
+    '2021-11-18T21:31:17.178Z',
+    '2021-12-23T07:42:02.383Z',
+    '2022-01-28T09:15:04.904Z',
+    '2022-04-01T10:17:24.185Z',
+    '2022-05-08T14:11:59.604Z',
+    '2022-05-27T17:01:17.194Z',
+    '2022-07-11T23:36:17.929Z',
+    '2022-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT',
 };
 
 const account4 = {
@@ -28,6 +67,19 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+
+  movementsDates: [
+    '2021-11-01T13:15:33.035Z',
+    '2021-11-30T09:48:16.867Z',
+    '2021-12-25T06:04:23.907Z',
+    '2022-01-25T14:18:46.235Z',
+    '2022-02-05T16:33:06.386Z',
+    '2022-04-10T14:43:26.374Z',
+    '2022-06-25T18:49:59.371Z',
+    '2022-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -75,9 +127,17 @@ const displayMovements = function (acc, sort = false) {
   //Display movements
   movs.forEach((mov, i) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const date = new Date(acc.movementsDates[i]);
+    const day = `${date.getDate()}`.padStart(2, '0');
+    const month = `${date.getMonth() + 1}`.padStart(2, '0');
+    const year = date.getFullYear();
+    const displayDate = `${day}/${month}/${year}`;
+
     const html = `
 	<div class="movements__row">
 		<div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+		<div class="movements__date">${displayDate}</div>
 		<div class="movements__value">${mov.toFixed(2)} $</div>
 	</div>`;
 
@@ -146,6 +206,12 @@ createUsernames(accounts);
 
 //Login
 let currentAcc;
+
+// //!FAKE ALWAYS LOGGED IN
+// currentAcc = account1;
+// updateUI(currentAcc);
+// containerApp.style.opacity = 100;
+
 btnLogin.addEventListener('click', e => {
   // Prevent form for submitting
   e.preventDefault();
@@ -157,6 +223,16 @@ btnLogin.addEventListener('click', e => {
     labelWelcome.textContent = `Welcome back ${currentAcc.owner.split(' ')[0]}`;
     containerApp.style.opacity = 100;
     inputLoginPin.blur(); //to lose a focus on input
+
+    //Current Date and Time
+    const now = new Date();
+    // day/month/year
+    const day = `${now.getDate()}`.padStart(2, '0');
+    const month = `${now.getMonth() + 1}`.padStart(2, '0');
+    const year = now.getFullYear();
+    const hour = `${now.getHours()}`.padStart(2, '0');
+    const minute = `${now.getMinutes()}`.padStart(2, '0');
+    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${minute}`;
 
     //clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
@@ -191,6 +267,10 @@ btnTransfer.addEventListener('click', e => {
     //Doing a transfer
     currentAcc.movements.push(-amount);
     receiverAcc.movements.push(amount);
+
+    //Add transfer Date
+    currentAcc.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
 
     //Update UI
     updateUI(currentAcc);
@@ -270,6 +350,9 @@ btnLoan.addEventListener('click', e => {
   if (amount > 0 && currentAcc.movements.some(mov => mov >= amount * 0.1)) {
     //Add mov
     currentAcc.movements.push(amount);
+
+    //Add loan date
+    currentAcc.movementsDates.push(new Date().toISOString());
 
     //Update UI
     updateUI(currentAcc);
